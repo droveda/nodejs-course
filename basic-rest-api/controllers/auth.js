cfbe38c7b1d2bb0c952bcf190c4c9e65ea484b37
dieguesroveda <dieguesroveda@gmail.com>
2026-08-20 22:25:46 -0300

    .

---
+++ basic-rest-api/controllers/auth.js
@@ -0,0 +1,22 @@
+const User = require('../models/user');
+
+const { validationResult } = require('express-validator');
+
+exports.signup = (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Failed. Entered data is incorrect!');
+        error.statusCode = 422;
+        error.data = errors.array();
+        throw error;
+    }
+
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+
+
+
+};
